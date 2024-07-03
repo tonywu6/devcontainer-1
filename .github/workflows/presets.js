af14a046a347da302e2c4efa5a23a691bebe2e97
Tony Wu <tonywu6@protonmail.com>
2024-07-03 08:26:10 +0000

    add glibc 2.28 variant

--- .github/workflows/presets.js
+++ .github/workflows/presets.js
@@ -1,51 +1,65 @@
-import * as process from 'node:process'
+import * as process from "node:process";
 
-const platforms = ['linux/arm64', 'linux/amd64']
+const platforms = ["linux/arm64", "linux/amd64"];
 
 const presets = {
   default: {
-    suffix: '',
+    suffix: "",
     latest: true,
     args: {},
   },
   manylinux_2_17: {
-    suffix: '-manylinux_2_17',
+    suffix: "-manylinux_2_17",
     latest: false,
     args: {
-      ZIGBUILD_GLIBC: '2.17',
-      DEFAULT_PYTHON_VERSION: '3.10',
-      DEFAULT_NODE_VERSION: '16',
-      DEFAULT_PNPM_VERSION: '8',
+      ZIGBUILD_GLIBC: "2.17",
+      DEFAULT_PYTHON_VERSION: "3.10",
+      DEFAULT_NODE_VERSION: "16",
+      DEFAULT_PNPM_VERSION: "8",
     },
     cross: {
-      'linux/arm64': 'quay.io/pypa/manylinux2014_aarch64:latest',
-      'linux/amd64': 'quay.io/pypa/manylinux2014_x86_64:latest',
+      "linux/arm64": "quay.io/pypa/manylinux2014_aarch64:latest",
+      "linux/amd64": "quay.io/pypa/manylinux2014_x86_64:latest",
     },
   },
-}
+  manylinux_2_28: {
+    suffix: "-manylinux_2_28",
+    latest: false,
+    args: {
+      ZIGBUILD_GLIBC: "2.28",
+      DEFAULT_PYTHON_VERSION: "3.10",
+      DEFAULT_NODE_VERSION: "20",
+      DEFAULT_PNPM_VERSION: "9",
+    },
+    cross: {
+      "linux/arm64": "quay.io/pypa/manylinux_2_28_aarch64:latest",
+      "linux/amd64": "quay.io/pypa/manylinux_2_28_x86_64:latest",
+    },
+  },
+};
 
 const builds = Object.entries(presets).flatMap(
   ([name, { suffix, args, cross, latest }]) =>
     platforms.map((platform) => {
-      const env = Object.entries(args)
+      const env = Object.entries(args);
       if (cross) {
-        env.push(['DEVCONTAINER_IMAGE', cross[platform]])
+        env.push(["DEVCONTAINER_IMAGE", cross[platform]]);
       }
-      const buildArgs = env.map(([key, value]) => `${key}=${value}`).join('\n')
-      return { name, platform, suffix, latest, 'build-args': buildArgs }
-    }),
-)
+      const buildArgs = env.map(([key, value]) => `${key}=${value}`).join("\n");
+      return { name, platform, suffix, latest, "build-args": buildArgs };
+    })
+);
 
 const tags = Object.entries(presets).flatMap(([name, { suffix, latest }]) => ({
   name,
   suffix,
   latest,
-}))
+}));
 
-process.stdout.write('builds<<EOF\n')
-process.stdout.write(JSON.stringify(builds, null, 2))
-process.stdout.write('\nEOF\n')
+process.stdout.write("builds<<EOF\n");
+process.stdout.write(JSON.stringify(builds, null, 2));
+process.stdout.write("\nEOF\n");
 
-process.stdout.write('tags<<EOF\n')
-process.stdout.write(JSON.stringify(tags, null, 2))
-process.stdout.write('\nEOF\n')
+process.stdout.write("tags<<EOF\n");
+process.stdout.write(JSON.stringify(tags, null, 2));
+process.stdout.write("\nEOF\n");
